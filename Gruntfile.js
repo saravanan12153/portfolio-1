02cba517c24f3ac2e839fb418f332e652f17a12b
Jon Samp <sampjon@gmail.com>
2016-02-03 21:06:02 -0600

    Added build control to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,6 +25,8 @@ module.exports = function (grunt) {
     dist: 'dist'
   };
 
+  grunt.loadNpmTasks('grunt-build-control');
+
   // Define the configuration for all the tasks
   grunt.initConfig({
 
