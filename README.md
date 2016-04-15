a50f7bd14cf62a3c98d9e1b7dd3c3a5f353ab0d8
Jon Samp <sampjon@gmail.com>
2016-04-14 19:58:20 -0500

    Performance improvements, now using service instead of Firebase

--- README.md
+++ README.md
@@ -13,13 +13,11 @@ You'll also see my life, week by week. It was inspired by [this article](http://
 ## Technologies
 This is made with AngularJS, one of my favorite frameworks to work with. I also generated most of the project with Yeoman's [yo angular generator](https://github.com/yeoman/generator-angular). If you haven't checked out Yeoman, it teleports you to the fun part of coding.
 
-In addition, the data you see is being served by [Firebase](https://www.firebase.com), which is a great solution for maintaining a database that can bind to an Angular app, and even update it in real time with its three-way data binding. This portfolio doesn't use the data binding in any real way, but it's nice to know it's there.
-
-Lastly, this app is built and served with [Grunt](http://gruntjs.com).
+Also, this app is built and served with [Grunt](http://gruntjs.com).
 
 ## Thoughts
-I should add that this project (right now) has no business being a full fledged AngularJS/Firebase app. Right now, it's like I bought a Ferrari to drive around my cul-de-sac. I *could* have made this with only HTML and CSS, but I went with AngularJS for a few reasons:
+I should add that this project (right now) has no business being a full fledged AngularJS app. Right now, it's like I bought a Ferrari to drive around my cul-de-sac. I *could* have made this with only HTML and CSS, but I went with AngularJS for a few reasons:
 
-1. The problem with portfolios is that they go out of date quickly, and remembering to update them is hard. —AngularJS can help here, as I can abstract out the data (as you can see, I have the portfolio.json, which is a replica of what is stored in a Firebase db), and edit it from afar, without mucking through the HTML every time.
+1. The problem with portfolios is that they go out of date quickly, and remembering to update them is hard. —AngularJS can help here, as I can abstract out the data (as you can see, I have the `portfolio.js` service, which holds all of the data), and edit it from afar, without mucking through the HTML every time.
 2. I want to teach people how to use AngularJS, so this little thing will one day become a fun tutorial for someone looking to learn AngularJS, and to showcase their work.
 3. And I just love AngularJS :heart_eyes:.
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,90 +8,85 @@
  * Controller of the desktopApp
  */
 angular.module('desktopApp')
-    .controller('MainCtrl', ['$firebaseObject', function($firebaseObject) {
+    .controller('MainCtrl', ['portfolioData', function(portfolioData) {
 
-        var ref = new Firebase('https://sweltering-fire-7706.firebaseio.com');
         var vm = this;
-        vm.profile = $firebaseObject(ref);
-        var lifeWeeks = $firebaseObject(ref);
+        vm.profile = portfolioData;
+        var lifeWeeks = portfolioData.lifeWeeks;
 
-        lifeWeeks.$loaded().then(function(data) {
-            var lifeWeeks = data.lifeWeeks;
+        // Array to put final object in
+        vm.weeks = [];
 
-            // Array to put final object in
-            vm.weeks = [];
+        // configuration
+        var startDate = Date.UTC(1989, 8, 18);
+        var weekLength = 6.048e+8;
+        var yearLength = 31449600000;
+        var endDate = Date.UTC(2089, 8, 19);
+        vm.mathFloor = Math.floor;
 
-            // configuration
-            var startDate = Date.UTC(1989, 8, 18);
-            var weekLength = 6.048e+8;
-            var yearLength = 31449600000;
-            var endDate = Date.UTC(2089, 8, 19);
-            vm.mathFloor = Math.floor;
+        // List every week
+        var weeksArray = [];
+        for (var i = startDate; i <= endDate; i += weekLength) {
+            weeksArray.push(i);
+        }
 
-            // List every week
-            var weeksArray = [];
-            for (var i = startDate; i <= endDate; i += weekLength) {
-                weeksArray.push(i);
+        // push the date to vm.weeks, and adds id
+        vm.weeks = weeksArray.map(function(week, index) {
+            return {
+                'id': index,
+                'date': week,
+                'type': '',
+                'description': '',
+                'class': [index],
+                'template': 'views/templates/general.tpl.html',
+                'value': ''
+            };
+        });
+
+        // Figure out if the week is in the past or in the future
+        // Get current week
+        var today = new Date();
+        var month = today.getMonth();
+
+        // Used + 1 since the weeks start on Monday
+        var startOfWeek = today.getDate() - today.getDay() + 1;
+        var year = today.getYear() + 1900;
+        var thisWeek = Date.UTC(year, month, startOfWeek);
+
+        // Add classes for past, present, future
+        for (var i = 0; i < vm.weeks.length; i++) {
+            if (thisWeek > vm.weeks[i].date) {
+                vm.weeks[i].class.push('past');
+            } else if (thisWeek === vm.weeks[i].date) {
+                vm.weeks[i].class.push('thisWeek');
+                vm.weeks[i].description = "This week."
+            } else {
+                vm.weeks[i].class.push('future');
             }
+        }
 
-            // push the date to vm.weeks, and adds id
-            vm.weeks = weeksArray.map(function(week, index) {
-                return {
-                    'id': index,
-                    'date': week,
-                    'type': '',
-                    'description': '',
-                    'class': [index],
-                    'template': 'views/templates/general.tpl.html',
-                    'value': ''
-                };
-            });
+        // Add class for birthdays
+        for (var i = 0; i < lifeWeeks.birthdayWeeks.length; i++) {
+            vm.weeks[lifeWeeks.birthdayWeeks[i]].class.push('birthday');
+            vm.weeks[lifeWeeks.birthdayWeeks[i]].value = i;
+            vm.weeks[lifeWeeks.birthdayWeeks[i]].template = 'views/templates/birthday.tpl.html';
+        }
 
-            // Figure out if the week is in the past or in the future
-            // Get current week
-            var today = new Date();
-            var month = today.getMonth();
+        // find my current age
+        var birthDate = new Date(1989, 9, 21);
+        var now = new Date();
+        var days = Math.floor((now - birthDate) / 86400000);
+        var myAge = days / 365.25;
+        vm.myAge = Math.floor(myAge);
 
-            // Used + 1 since the weeks start on Monday
-            var startOfWeek = today.getDate() - today.getDay() + 1;
-            var year = today.getYear() + 1900;
-            var thisWeek = Date.UTC(year, month, startOfWeek);
+        // Adds color to weeks after life expectancy;
+        for (var i = 4123; i < vm.weeks.length; i++) {
+            vm.weeks[i].class.push('after-life');
+        }
 
-            // Add classes for past, present, future
-            for (var i = 0; i < vm.weeks.length; i++) {
-                if (thisWeek > vm.weeks[i].date) {
-                    vm.weeks[i].class.push('past');
-                } else if (thisWeek === vm.weeks[i].date) {
-                    vm.weeks[i].class.push('thisWeek');
-                    vm.weeks[i].description = "This week."
-                } else {
-                    vm.weeks[i].class.push('future');
-                }
-            }
-
-            // Add class for birthdays
-            for (var i = 0; i < lifeWeeks.birthdayWeeks.length; i++) {
-                vm.weeks[lifeWeeks.birthdayWeeks[i]].class.push('birthday');
-                vm.weeks[lifeWeeks.birthdayWeeks[i]].value = i;
-                vm.weeks[lifeWeeks.birthdayWeeks[i]].template = 'views/templates/birthday.tpl.html';
-            }
-
-            // find my current age
-            var birthDate = new Date(1989, 9, 21);
-            var now = new Date();
-            var days = Math.floor((now - birthDate) / 86400000);
-            var myAge = days / 365.25;
-            vm.myAge = Math.floor(myAge);
-
-            // Adds color to weeks after life expectancy;
-            for (var i = 4123; i < vm.weeks.length; i++) {
-                vm.weeks[i].class.push('after-life');
-            }
-
-            // Adds events to week calendar from lifeWeeks object
-            lifeWeeks.events.map(function(event) {
-                vm.weeks[event.id].class.push(event.class);
-                vm.weeks[event.id].description = event.description;
-            });
+        // Adds events to week calendar from lifeWeeks object
+        lifeWeeks.events.map(function(event) {
+            vm.weeks[event.id].class.push(event.class);
+            vm.weeks[event.id].description = event.description;
         });
     }]);
