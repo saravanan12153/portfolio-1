14734b5514e57e506ea069242284ca6d4976b034
Jon Samp <sampjon@gmail.com>
2016-06-04 23:31:32 -0500

    Added coming soon image

--- app/scripts/controllers/coffee.js
+++ app/scripts/controllers/coffee.js
@@ -14,13 +14,25 @@ angular.module('desktopApp')
             'AngularJS',
             'Karma'
         ];
-
+        'use strict';
         var vm = this;
 
+        // Initializes the weather on load
         weather.then(function(data) {
-            vm.currentWeather = ((data.data.main.temp - 273) * (9 / 5)) + 32;
-        });
+            vm.currentWeather = data.data.current_observation.temp_f;
+            vm.currentCondition = data.data.current_observation.weather.toLowerCase();
+        })
 
+        // Updates the weather every 7 minutes
+        setInterval(function(){
+            weather.then(function(data) {
+                vm.currentWeather = data.data.current_observation.temp_f;
+                vm.currentCondition = data.data.current_observation.weather.toLowerCase();
+            })
+        }, 420000);
+
+
+        // Gets time working, and updating every second.
         function updateTime() {
             var currentTime = new Date();
             var hours = currentTime.getHours();
@@ -28,11 +40,15 @@ angular.module('desktopApp')
             if (minutes < 10) {
                 minutes = "0" + minutes;
             }
-            var t_str = hours + ":" + minutes + " ";
+            var t_str = hours + ":" + minutes;
+
             if (hours > 11) {
                 t_str = (hours - 12) + ":" + minutes;
                 t_str += "PM";
-            } else {
+            } else if (hours === 0) {
+                t_str = '12:' + minutes + "AM";
+
+            }else {
                 t_str += "AM";
             }
 
--- app/scripts/services/weather.js
+++ app/scripts/services/weather.js
@@ -1,12 +1,15 @@
 angular.module('desktopApp')
     .factory('weather', ['$http', function($http) {
-        var latitude = 41.903412878282225;
-        var longitude = -87.67159575682756;
-        var API_KEY = "01b46659448bc9aa3636d754bf36a1bd";
-        return $http.get('http://api.openweathermap.org/data/2.5/weather' +
-                "?lat=" + latitude +
-                "&lon=" + longitude +
-                "&appid=" + API_KEY)
+        var latitude = 41.9035041;
+        var longitude = -87.67137919999999;
+        var API_KEY = "8f4fcb7cc3c8f50f";
+        return $http.get('http://api.wunderground.com/api/' +
+                API_KEY + "/" +
+                "conditions/geolookup/q/" +
+                latitude + "," +
+                longitude +
+                ".json"
+                )
                 .success(function(response) {
                     console.log(response.data);
                     return response.data;
